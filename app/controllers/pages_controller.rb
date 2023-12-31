242b1ea08e68625cdb2c10d29a7a9c6c68e71596
Kieran Huybrechts <kieranhuybrechts@gmail.com>
2023-12-31 12:10:54 +0200

    save

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,5 @@ class PagesController < ApplicationController
 
   def home
     @posts = Post.all
-    #Need to correct this because this doesn't give the right order!
-    @posts_embed_urls = []
-    @posts.each do |post|
-      song = post.songs.first
-      match_data = /spotify\.com\/track\/([^?]+)/.match(song.spotify_url)
-      track_id = match_data[1] if match_data.present?
-      embedded_url = "https://open.spotify.com/embed/track/#{track_id}?utm_source=generator&theme=0"
-      @posts_embed_urls << embedded_url
-    end
-
   end
 end
